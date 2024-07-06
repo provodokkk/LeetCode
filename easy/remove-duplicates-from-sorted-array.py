d355358691dede99982d8329a57baa0692d0b15b
provodokkk <danylo.provodov@gmail.com>
2024-07-06 16:05:17 +0200

    Remove Duplicates from Sorted Array - initial commit

---
+++ easy/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(n)
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1
+
+        for i in range(len(nums) - 1):
+            if nums[i] != nums[i + 1]:
+                nums[k] = nums[i + 1]
+                k += 1
+
+        return k
